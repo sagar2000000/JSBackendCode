ee1f5a1692bde46db90e429144bc226057861974
sagar2000000 <sagar171joshi@gmail.com>
2024-07-15 21:20:24 +0545

    subscriptionModel created

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -167,8 +167,7 @@ const refereshAccessToken = asyncHandler(async (req, res) => {
   if (!incomingRefreshToken) {
     throw new ApiError(401, "unauthorized request");
   }
-try {
-  
+  try {
     const decodedToken = jwt.verify(
       incomingRefreshToken,
       process.env.REFRESH_TOKEN_SECRET
@@ -180,24 +179,129 @@ try {
     if (incomingRefreshToken !== user?.refreshToken) {
       throw new ApiError(401, " refresh token is expired or used");
     }
-  
+
     const options = {
       httpOnly: true,
       secure: true,
     };
-    const { accessToken, newrefreshToken } = await generateAccessAndRefreshTokens(
-      user._id
-    );
+    const { accessToken, newrefreshToken } =
+      await generateAccessAndRefreshTokens(user._id);
     return res
       .status(200)
       .cookie("accessToken", accessToken, options)
       .cookie("refreshToken", newrefreshToken, options)
       .json(
-        new ApiResponse(200,{accessToken,newrefreshToken},"new referesh token created")
-      )
-} catch (error) {
-  throw new ApiError(401,error?.message||"Invalid decoding")
-}
+        new ApiResponse(
+          200,
+          { accessToken, newrefreshToken },
+          "new referesh token created"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid decoding");
+  }
 });
 
-export { registerUser, loginUser, logoutUser,refereshAccessToken };
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  const user = await User.findById(req.user?._id);
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, "Invalid old password")
+  }
+    user.password = newPassword
+    await user.save({ validateBeforeSave: false });
+
+    return res.status(200).json(new ApiResponse(200, {}, "password changed sucessfully"));
+  }
+);
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json( new ApiResponse(200, req.user, "Current user fetched sucessfully"));
+});
+
+const updateAccountDetails = asyncHandler((req, res) => {
+  const { fullName, email } = req.body;
+
+  if (!fullName || !email) {
+    throw new ApiError(400, "All fields are required");
+  }
+  const user = User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        fullName,
+        email,
+      },
+    },
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated sucessfully"));
+});
+
+const updateUserAvatar = asyncHandler(async (req, res) => {
+  const avatarLocalPath = req.file?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is missing");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  if (!avatar.url) {
+    throw new ApiError(400, "Error while uploading on avatar");
+  }
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        avatar: avatar.url,
+      },
+    },
+    { new: true }
+  ).select("-password");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "avatar updated sucessfully"));
+});
+
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+  const coverImageLocalPath = req.file?.path;
+  if (!coverImageLocalPath) {
+    throw new ApiError(400, "cover image file is missing");
+  }
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  if (!coverImage.url) {
+    throw new ApiError(400, "Error while uploading on cover image");
+  }
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        coverImage: coverImage.url,
+      },
+    },
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "CoverImage updated sucessfully"));
+});
+
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refereshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+};
---
+++ src/models/subscription.model.js
@@ -0,0 +1,22 @@
+import mongoose ,{Schema} from "mongoose";
+
+const subscriptionSchema=new Schema({
+  subscriber:{
+    type:Schema.Types.ObjectId, //one who is subscribing 
+
+    ref:"User"
+
+  },
+  channel:{
+    type:Schema.Types.ObjectId, //one whose channel is subscribed 
+
+    ref:"User"
+
+  }
+},{timestamps:true})
+
+
+
+
+
+export const Subcription=mongoose.model("Subscription",subscriptionSchema)
